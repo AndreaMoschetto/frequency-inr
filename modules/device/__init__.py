5d5a0f4d607d96afdfeb7b55f387606e380aea99
AndreaMoschetto <andreamoschetto99@gmail.com>
2025-12-29 22:23:53 +0100

    feat: autodetect mps device

--- modules/device/__init__.py
+++ modules/device/__init__.py
@@ -1,9 +1,26 @@
 import torch
 import os
+from modules.logging import init_logger
+
+LOGGER = init_logger(__name__)
 
 
 def load_device(force_cpu=False):
     if force_cpu:
-        torch.device("cpu")
+        LOGGER.info("Device forced to CPU.")
+        return torch.device("cpu")
+
+    env_device = os.environ.get("DEVICE")
+    if env_device:
+        LOGGER.info(f"Using device from env: {env_device}")
+        return torch.device(env_device)
+
+    if torch.cuda.is_available():
+        LOGGER.info("CUDA detected. Using NVIDIA GPU.")
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        LOGGER.info("Apple Silicon (MPS) detected. Using Metal Performance Shaders.")
+        return torch.device("mps")
     else:
-        return torch.device(os.environ.get("DEVICE") or "cuda")
+        LOGGER.info("No GPU detected. Falling back to CPU.")
+        return torch.device("cpu")
